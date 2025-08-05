787922e2e173352dc23fd7beac89a835720f4c0e
Stanislav Kovalev <djently@yandex.ru>
2025-08-05 14:58:31 +0300

    add vk tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Building2 } from "lucide-react";
 import YM from "@/components/YM";
+import VK from "@/components/VK";
 import "./globals.css";
 
 const Footer = dynamic(() => import("@/components/Footer"));
@@ -52,6 +53,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <YM />
+        <VK />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
